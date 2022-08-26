152a5b137eb4a3283852629e6fe8cb4914c34828
Yaroslavpnts <rofem2335@gmail.com>
2022-08-26 11:34:35 +0300

    corrected protocol (from http to https) and added catching exceptions

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -23,11 +23,15 @@ class Main extends React.Component {
   getMovies(str = 'matrix', type = 'all') {
     this.setState({ isLoading: true });
     fetch(
-      `http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !== 'all' ? `&type=${type}` : ''}`
+      `https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !== 'all' ? `&type=${type}` : ''}`
     )
       .then(response => response.json())
       .then(data => {
         this.setState({ movies: data.Search, isLoading: false });
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({ isLoading: false });
       });
   }
 
